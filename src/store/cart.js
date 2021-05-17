49236367a50a1cf0da670c4ab617dde697d9a93f
thenatural86 <philip.smalls86@gmail.com>
2021-05-17 17:21:21 -0400

    add item to cart

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,11 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialCartState = { count: 0 }
+const initialCartState = { items: [], totalQuantity: 0, count: 0 }
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: initialCartState,
   reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload
+      const existingItem = state.items.find((item) => item.id === newItem.id)
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        })
+      } else {
+        existingItem.quantity++
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+      }
+    },
+
+    removeItemFromCart(state, action) {},
     increment(state) {
       state.count++
     },
