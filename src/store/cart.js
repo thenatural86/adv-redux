fc30e3615961fc3906eb291422373a92e221e12d
thenatural86 <philip.smalls86@gmail.com>
2021-05-19 11:02:24 -0400

    updated add to cart handler

--- src/store/cart.js
+++ src/store/cart.js
@@ -6,6 +6,10 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState: initialCartState,
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity
+      state.items = action.payload.items
+    },
     addItemToCart(state, action) {
       const newItem = action.payload
       const existingItem = state.items.find((item) => item.id === newItem.id)
