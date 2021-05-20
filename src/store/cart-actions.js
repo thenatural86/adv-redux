58abcae74d26e3018f6d3e7a0271c2952a53e935
thenatural86 <philip.smalls86@gmail.com>
2021-05-20 14:08:13 -0400

    finalizing async and side effect code cleanup

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -20,7 +20,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData()
-      dispatch(cartActions.replaceCart(cartData))
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      )
     } catch (error) {
       dispatch(
         toggleActions.showNotification({
@@ -48,7 +53,10 @@ export const sendCartData = (cart) => {
         'https://react-movies-97451-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       )
       if (!response.ok) {
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialCartState = { items: [], totalQuantity: 0 }
+const initialCartState = { items: [], totalQuantity: 0, changed: false }
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -14,6 +14,7 @@ const cartSlice = createSlice({
       const newItem = action.payload
       const existingItem = state.items.find((item) => item.id === newItem.id)
       state.totalQuantity++
+      state.changed = true
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -32,6 +33,8 @@ const cartSlice = createSlice({
       const id = action.payload
       const existingItem = state.items.find((item) => item.id === id)
       state.totalQuantity--
+      state.changed = true
+
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id)
       } else {
